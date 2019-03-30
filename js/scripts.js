eb7a82416ceadf9d7e2874da2c83ac919bddd37d
Elena <elenacellitti92@gmail.com>
2019-03-30 17:19:16 +0000

    All recipe pages created and linked

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,11 @@ $(function() {
 //the ingredients and method appear when user clicks 'tell me more' button
 $(".recipe_1_btn.lets_try").click(function(){
   $(".recipe_1").show(10);
-  $('.recipe_2').hide()
+  $('.recipe_2').hide;
+  window.scrollBy(1000, 0);
 });
   $(".recipe_2_btn.lets_try").click(function(){
   $(".recipe_2").show(10);
   $('.recipe_1').hide();
+  window.scrollBy(1000, 0);
 });
