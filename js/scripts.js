c8f5b88bf20d941682ecace7e7a9def85cef237c
Elena <elenacellitti92@gmail.com>
2019-04-01 19:47:57 +0100

    Added titles to all the pages

--- js/scripts.js
+++ js/scripts.js
@@ -6,13 +6,13 @@ $(function() {
 
 
 //the ingredients and method appear when user clicks 'tell me more' button
-$(".recipe_1_btn.lets_try").click(function(){
-  $(".recipe_1").show(10);
+$('.recipe_1_btn.lets_try').click(function(){
+  $('.recipe_1').show(10);
   $('.recipe_2').hide;
-  window.scrollBy(1000, 0);
+  $('.recipe_1').scrollIntoView(true);
 });
-  $(".recipe_2_btn.lets_try").click(function(){
-  $(".recipe_2").show(10);
+  $('.recipe_2_btn.lets_try').click(function(){
+  $('.recipe_2').show(10);
   $('.recipe_1').hide();
-  window.scrollBy(1000, 0);
+  $('.recipe_2').scrollIntoView(true);
 });
