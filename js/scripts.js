1bae277229d745fc4e69ee3bcc91e2da7862a542
Elena <elenacellitti92@gmail.com>
2019-04-02 19:45:51 +0100

    Adding scroll function to scripts.js for recipes

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,30 @@
+const speed = 1000;
+const speed2 = 500;
+
+function smoothScroll(to) {
+  $('html, body').animate(
+    {
+      scrollTop: $(to).offset().top,
+    },
+    speed,
+    'linear'
+  );
+}
+
 //hide all recipes as default
 $(function() {
   $('.recipe_1').hide();
-  $('.recipe_2').hide()
-});
+  $('.recipe_2').hide();
 
-
-//the ingredients and method appear when user clicks 'tell me more' button
-$('.recipe_1_btn.lets_try').click(function(){
-  $('.recipe_1').show(10);
-  $('.recipe_2').hide;
-  $('.recipe_1').scrollIntoView(true);
-});
+  //the ingredients and method appear when user clicks 'tell me more' button
+  $('.recipe_1_btn.lets_try').click(function(){
+    $('.recipe_1').show(speed);
+    $('.recipe_2').hide(speed2);
+    smoothScroll('.recipe_1');
+  });
   $('.recipe_2_btn.lets_try').click(function(){
-  $('.recipe_2').show(10);
-  $('.recipe_1').hide();
-  $('.recipe_2').scrollIntoView(true);
+    $('.recipe_2').show(speed);
+    $('.recipe_1').hide(speed2);
+    smoothScroll('.recipe_2');
+  });
 });
